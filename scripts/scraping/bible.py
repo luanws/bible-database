b39964028e63fbd79053794a16cd3e30414bc2f7
Luan Willig Silveira <luan.w.silveira@gmail.com>
2024-07-13 01:34:10 -0300

    scraping em paralelo

--- scripts/scraping/bible.py
+++ scripts/scraping/bible.py
@@ -2,33 +2,96 @@ import codecs
 import json
 import os
 import re
+from concurrent.futures import ThreadPoolExecutor
 from itertools import count
 
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'https://www.bible.com/bible'
-versions_url = 'https://www.bible.com/versions'
-language = input('Language: ')  # Example: en
-version_name = input('Version name: ')  # Example: 'ARA'
+base_url = "https://www.bible.com/bible"
+versions_url = "https://www.bible.com/versions"
+language = input("Language: ")  # Example: en
+version_name = input("Version name: ")  # Example: 'ARA'
 book_chapters = {
-    'GEN': 50, 'EXO': 40, 'LEV': 27, 'NUM': 36, 'DEU': 34, 'JOS': 24,
-    'JDG': 21, 'RUT': 4, '1SA': 31, '2SA': 24, '1KI': 22, '2KI': 25,
-    '1CH': 29, '2CH': 36, 'EZR': 10, 'NEH': 13, 'EST': 10, 'JOB': 42,
-    'PSA': 150, 'PRO': 31, 'ECC': 12, 'SNG': 8, 'ISA': 66, 'JER': 52,
-    'LAM': 5, 'EZK': 48, 'DAN': 12, 'HOS': 14, 'JOL': 3, 'AMO': 9,
-    'OBA': 1, 'JON': 4, 'MIC': 7, 'NAM': 3, 'HAB': 3, 'ZEP': 3,
-    'HAG': 2, 'ZEC': 14, 'MAL': 4, 'MAT': 28, 'MRK': 16, 'LUK': 24,
-    'JHN': 21, 'ACT': 28, 'ROM': 16, '1CO': 16, '2CO': 13, 'GAL': 6,
-    'EPH': 6, 'PHP': 4, 'COL': 4, '1TH': 5, '2TH': 3, '1TI': 6,
-    '2TI': 4, 'TIT': 3, 'PHM': 1, 'HEB': 13, 'JAS': 5, '1PE': 5,
-    '2PE': 3, '1JN': 5, '2JN': 1, '3JN': 1, 'JUD': 1, 'REV': 22
+    "GEN": 50,
+    "EXO": 40,
+    "LEV": 27,
+    "NUM": 36,
+    "DEU": 34,
+    "JOS": 24,
+    "JDG": 21,
+    "RUT": 4,
+    "1SA": 31,
+    "2SA": 24,
+    "1KI": 22,
+    "2KI": 25,
+    "1CH": 29,
+    "2CH": 36,
+    "EZR": 10,
+    "NEH": 13,
+    "EST": 10,
+    "JOB": 42,
+    "PSA": 150,
+    "PRO": 31,
+    "ECC": 12,
+    "SNG": 8,
+    "ISA": 66,
+    "JER": 52,
+    "LAM": 5,
+    "EZK": 48,
+    "DAN": 12,
+    "HOS": 14,
+    "JOL": 3,
+    "AMO": 9,
+    "OBA": 1,
+    "JON": 4,
+    "MIC": 7,
+    "NAM": 3,
+    "HAB": 3,
+    "ZEP": 3,
+    "HAG": 2,
+    "ZEC": 14,
+    "MAL": 4,
+    "MAT": 28,
+    "MRK": 16,
+    "LUK": 24,
+    "JHN": 21,
+    "ACT": 28,
+    "ROM": 16,
+    "1CO": 16,
+    "2CO": 13,
+    "GAL": 6,
+    "EPH": 6,
+    "PHP": 4,
+    "COL": 4,
+    "1TH": 5,
+    "2TH": 3,
+    "1TI": 6,
+    "2TI": 4,
+    "TIT": 3,
+    "PHM": 1,
+    "HEB": 13,
+    "JAS": 5,
+    "1PE": 5,
+    "2PE": 3,
+    "1JN": 5,
+    "2JN": 1,
+    "3JN": 1,
+    "JUD": 1,
+    "REV": 22,
 }
 book_ids = list(book_chapters.keys())
 
 
 class Chapter:
-    def __init__(self, version_id: str, version_name: str, book_index: int, chapter_index: int, book_id: str):
+    def __init__(
+        self,
+        version_id: str,
+        version_name: str,
+        book_index: int,
+        chapter_index: int,
+        book_id: str,
+    ):
         self.version_id = version_id
         self.version_name = version_name
         self.book_index = book_index
@@ -48,10 +111,10 @@ class Verse:
 
     def __dict__(self):
         return {
-            'book': self.chapter.book_id,
-            'chapter': self.chapter.chapter_number,
-            'verse': self.verse_number,
-            'text': self.verse_text
+            "book": self.chapter.book_id,
+            "chapter": self.chapter.chapter_number,
+            "verse": self.verse_number,
+            "text": self.verse_text,
         }
 
 
@@ -61,12 +124,12 @@ def get_book_id_from_chapter(chapter: Chapter) -> str:
 
 def make_chapter_url(chapter: Chapter) -> str:
     book_id = get_book_id_from_chapter(chapter)
-    return f'{base_url}/{chapter.version_id}/{book_id}.{chapter.chapter_number}.{chapter.version_name}'
+    return f"{base_url}/{chapter.version_id}/{book_id}.{chapter.chapter_number}.{chapter.version_name}"
 
 
 def get_soup_from_url(url: str) -> BeautifulSoup:
     response = requests.get(url)
-    return BeautifulSoup(response.text, 'html.parser')
+    return BeautifulSoup(response.text, "html.parser")
 
 
 def create_directory_if_not_exists(directory: str):
@@ -77,30 +140,39 @@ def create_directory_if_not_exists(directory: str):
 def save_verses_in_json(filename: str, verses: list[Verse]):
     create_directory_if_not_exists(os.path.dirname(filename))
     verses_json = json.dumps([verse.__dict__() for verse in verses])
-    with open(filename, 'w', encoding='utf-8') as f:
-        content = codecs.decode(verses_json, 'unicode_escape')
+    with open(filename, "w", encoding="utf-8") as f:
+        content = codecs.decode(verses_json, "unicode_escape")
         f.write(content)
 
 
-def get_all_chapters(book_ids: list[str], book_chapters: dict[str, int], version_id: str, version_name: str) -> list[Chapter]:
-    return [Chapter(
-        version_id=version_id,
-        version_name=version_name,
-        book_index=book_index,
-        chapter_index=chapter_index,
-        book_id=book_ids[book_index]
-    ) for book_index in range(len(book_ids)) for chapter_index in range(book_chapters[book_ids[book_index]])]
+def get_all_chapters(
+    book_ids: list[str],
+    book_chapters: dict[str, int],
+    version_id: str,
+    version_name: str,
+) -> list[Chapter]:
+    return [
+        Chapter(
+            version_id=version_id,
+            version_name=version_name,
+            book_index=book_index,
+            chapter_index=chapter_index,
+            book_id=book_ids[book_index],
+        )
+        for book_index in range(len(book_ids))
+        for chapter_index in range(book_chapters[book_ids[book_index]])
+    ]
 
 
 def scrape_version_id(versions_url: str, version_name: str) -> str:
     if not version_name:
-        raise Exception('Version name skipped')
+        raise Exception("Version name skipped")
     soup = get_soup_from_url(versions_url)
-    version_a = soup.find('a', text=re.compile(f'\\({version_name}\\)'))
-    href = version_a['href']
-    search = re.search(r'/versions/(\d+)-', href)
+    version_a = soup.find("a", text=re.compile(f"\\({version_name}\\)"))
+    href = version_a["href"]
+    search = re.search(r"/versions/(\d+)-", href)
     if not isinstance(search, re.Match):
-        raise Exception('Version not found')
+        raise Exception("Version not found")
     version_id: str = search.group(1)
     return version_id
 
@@ -109,16 +181,18 @@ def scrape_chapter(chapter: Chapter, chapter_url: str) -> list[Verse]:
     soup = get_soup_from_url(chapter_url)
     verses: list[Verse] = []
     for verse_number in count(1):
-        verse_reference = f'{chapter.book_id}.{chapter.chapter_number}.{verse_number}'
-        verse_spans = soup.find_all('span', attrs={'data-usfm': verse_reference})
+        verse_reference = f"{chapter.book_id}.{chapter.chapter_number}.{verse_number}"
+        verse_spans = soup.find_all("span", attrs={"data-usfm": verse_reference})
         if not verse_spans:
             break
         partial_verse_texts: list[str] = []
         for verse_span in verse_spans:
-            content_verse_spans = verse_span.find_all('span', attrs={'class': 'content'})
-            partial_verse_text = ''.join([v.text for v in content_verse_spans])
+            content_verse_spans = verse_span.find_all(
+                "span", attrs={"class": "content"}
+            )
+            partial_verse_text = "".join([v.text for v in content_verse_spans])
             partial_verse_texts.append(partial_verse_text)
-        verse_text = ' '.join(partial_verse_texts)
+        verse_text = " ".join(partial_verse_texts)
         verse = Verse(chapter, verse_number, verse_text)
         verses.append(verse)
     return verses
@@ -126,26 +200,36 @@ def scrape_chapter(chapter: Chapter, chapter_url: str) -> list[Verse]:
 
 try:
     version_id = scrape_version_id(versions_url, version_name)
-    print(f'Version ID: {version_id}')
+    print(f"Version ID: {version_id}")
 except Exception as e:
     if version_name:
-        print(f'Version {version_name} not found. Please insert the version ID manually.')
-        version_id = input('Version ID: ')
+        print(
+            f"Version {version_name} not found. Please insert the version ID manually."
+        )
+        version_id = input("Version ID: ")
     else:
-        print('Version name skipped. Please insert the version ID manually.')
-        version_id = input('Version ID: ')
-        version_name = input('Version name: ')
+        print("Version name skipped. Please insert the version ID manually.")
+        version_id = input("Version ID: ")
+        version_name = input("Version name: ")
+
+
+def scrape_verses_by_chapter(chapter: Chapter) -> list[Verse]:
+    chapter_url = make_chapter_url(chapter)
+    print(f"Getting {chapter_url}")
+    chapter_verses = scrape_chapter(chapter, chapter_url)
+    print(f"Scraped {chapter.book_id} {chapter.chapter_number}:1-{len(chapter_verses)}")
+    return chapter_verses
 
 
 all_chapters = get_all_chapters(book_ids, book_chapters, version_id, version_name)
-print(f'{len(all_chapters)} chapters generated')
+print(f"{len(all_chapters)} chapters generated")
 
 verses: list[Verse] = []
-for chapter in all_chapters:
-    chapter_url = make_chapter_url(chapter)
-    print(f'Getting {chapter_url}')
-    chapter_verses = scrape_chapter(chapter, chapter_url)
-    print(f'Scraped {chapter.book_id} {chapter.chapter_number}:1-{len(chapter_verses)}')
+
+with ThreadPoolExecutor() as executor:
+    results = list(executor.map(scrape_verses_by_chapter, all_chapters))
+
+for chapter_verses in results:
     verses.extend(chapter_verses)
 
-save_verses_in_json(f'data/json/{language}/{version_name}.json', verses)
+save_verses_in_json(f"data/json/{language}/{version_name}.json", verses)
